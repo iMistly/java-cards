3ebd497d97ab5e2cdc30398f50f84359127a0c16
Stephen Garza <craftyminer22@gmail.com>
2025-04-23 16:10:38 -0500

    Create basic deck

--- src/cardgames/Card.java
+++ src/cardgames/Card.java
@@ -1,8 +1,37 @@
 package cardgames;
-
 // Removing 'public' makes the Deck class package-private
 class Card {
-    char suit; // (C)lubs, (D)iamonds, (H)earts, (S)pades
+    public static final String[] VALID_RANKS = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    public static final String[] VALID_SUITS = {"C", "H", "S", "D"};
+
     String rank; // (A)ce, 2, 3, 4, 5, 6, 7, 8, 9, 10, (J)ack, (Q)ueen, (K)ing
-    public Card(){}
+    String suit; // (C)lubs, (H)earts, (S)pades, (D)iamonds
+
+    // Ensure that the requested card is valid
+    public Card(String rank, String suit){
+        if (isValidRank(rank) && isValidSuit(suit)) {
+            this.rank = rank;
+            this.suit = suit;
+        } else {
+            throw new IllegalArgumentException("Invalid card: " + rank + suit);
+        }
+    }
+    
+    private boolean isValidRank(String rank) {
+        for (String validRank : VALID_RANKS) {
+            if (validRank.equals(rank)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isValidSuit(String suit) {
+        for (String validSuit : VALID_SUITS) {
+            if (validSuit.equals(suit)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/cardgames/Deck.java
+++ src/cardgames/Deck.java
@@ -2,5 +2,22 @@ package cardgames;
 
 // Removing 'public' makes the Deck class package-private
 class Deck {
-    public Deck(){}
+    Card[] cards = new Card[52];
+    int topCard = 0; // Index of the top card in the deck
+    
+    // Constructor to initialize the deck with 52 cards
+    public Deck(){
+        int index = 0;
+        for (String suit : Card.VALID_SUITS) {
+            for (String rank : Card.VALID_RANKS) {
+                cards[index++] = new Card(rank, suit);
+            }
+        }
+    }
+
+    public void show_deck(){
+        for (int i = 0; i < cards.length; i++) {
+            System.out.print(cards[i].rank + cards[i].suit + " ");
+        }
+    }
 }
